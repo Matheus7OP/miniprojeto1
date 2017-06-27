7d994c7d0d19a8f2a1ed6ad0c77185d762a7f9ac
Matheus Oliveira <Matheus7op@gmail.com>
2017-06-26 22:32:53 -0300

    Create damas.py

---
+++ damas.py
@@ -0,0 +1,202 @@
+# coding: utf-8
+# Aluno: Matheus Oliveira
+# Disciplina: Programação I
+# Miniprojeto Damas
+
+import os, sys
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+# início das constantes
+BOARD_SIZE = 8
+PIECE_RADIUS = 20
+
+BOARD_COLOR_1 = (192, 108, 37)
+BOARD_COLOR_2 = (236, 174, 118)
+
+PLAYER1_COLOR = (23, 98, 161)
+PLAYER2_COLOR = (125, 202, 92)
+BLACK = (0, 0, 0)
+# fim das constantes.
+
+class Piece(pygame.sprite.Sprite):
+	def __init__(self, player, position, piece_id):
+		colors = [PLAYER1_COLOR, PLAYER2_COLOR]
+		
+		self.color = colors[player-1]
+		self.player = player
+		
+		self.position = position
+		self.coord = [position[0]/60, position[1]/60]
+		
+		self.ID = piece_id
+		self.status = 'alive'
+		
+	def draw_piece(self, gameBoard):
+		pygame.draw.circle(gameBoard, self.color, [self.coord[0]*60+30, self.coord[1]*60+30], PIECE_RADIUS)
+		
+	def check_movement(self, new_coord, gameBoard):
+		column = new_coord[0]
+		row = new_coord[1]
+		
+		if column < 0 or column > 7:
+			return False
+		if row < 0 or row > 7:
+			return False
+		
+		if self.player == 2:
+			if row != self.coord[1]-1:
+				return False
+			if column != self.coord[0]+1 and column != self.coord[0]-1:
+				return False
+		else:
+			if row != self.coord[1]+1:
+				return False
+			if column != self.coord[0]+1 and column != self.coord[0]-1:
+				return False
+			
+		if gameBoard.board_status[row][column] != 0:
+			return False
+		
+		return True
+	
+	def move(self, new_coord, gameBoard):
+		row = self.coord[1]
+		column = self.coord[0]
+		
+		gameBoard.board_status[row][column] = 0
+		self.coord = new_coord
+		
+		row = self.coord[1]
+		column = self.coord[0]
+		gameBoard.board_status[row][column] = self.player
+		
+
+class Board(pygame.sprite.Sprite):
+	def __init__(self, board_size):
+		self.colors = [BOARD_COLOR_1, BOARD_COLOR_2]	# cores do quadriculado alternado.
+		
+		self.grid_size = board_size		# tamanho do tabuleiro (n x n).
+		self.surface_size = 480	# tamanho da tela em pixels.
+		
+		self.square_size = 60
+		self.surface = pygame.display.set_mode((self.surface_size, self.surface_size)) # +220 1ºp (MENU)
+		
+		self.board_status = []
+		self.amount_pieces = 0
+		
+		for i in xrange(board_size):
+			self.board_status.append([])
+			for j in xrange(0, board_size):
+				self.board_status[i].append(0)
+				
+		for i in xrange(board_size):	# adicionando peças iniciais ao tabuleiro.
+			if i == 3 or i == 4: continue
+			
+			if(i <= 3): player = 1
+			else: player = 2
+			
+			ini = i%2	
+			
+			for j in xrange(ini, board_size, 2):
+				if player == 1: 
+					self.board_status[i][j] = 1
+				elif player == 2:
+					self.board_status[i][j] = 2
+		
+	def initialize_board(self):
+		for row in xrange(self.grid_size):
+			actual_color = row%2
+			
+			for column in xrange(self.grid_size):	# desenhando o tabuleiro
+				square = (column*self.square_size, row*self.square_size, self.square_size, self.square_size)
+				
+				self.surface.fill(self.colors[actual_color], square)
+				actual_color = (actual_color+1)%2
+	
+	def get_info(self):
+		print 'Board status: '
+		
+		for i in xrange(self.grid_size):	# exibição do status atual do tabuleiro ao programador.
+			print self.board_status[i]
+		
+		print
+		
+	def initialize_pieces(self):
+		self.pieces = []
+		
+		for row in xrange(self.grid_size):
+
+			actual_color = row%2
+			
+			if(row <= 3): player = 1
+			else: player = 2
+			
+			for column in xrange(self.grid_size):
+				if self.board_status[row][column] != 0:
+					new_piece = Piece(player, [column*self.square_size+30, row*self.square_size+30], self.amount_pieces)
+					new_piece.draw_piece(self.surface)
+					
+					self.pieces.append(new_piece)
+					self.amount_pieces += 1
+	
+	def update_board(self):
+		area = (0, 0, self.surface_size, self.surface_size)
+		self.surface.fill(BLACK, area)
+		
+		self.initialize_board()
+		
+		for piece in self.pieces:
+			if piece.status == 'alive':
+				piece.draw_piece(self.surface)
+						
+						
+gameBoard = Board(BOARD_SIZE)
+sqr_size = gameBoard.square_size
+gameBoard.initialize_pieces()
+
+pygame.display.set_caption('Damas')
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+	event = pygame.event.wait()
+	
+	if event.type == pygame.QUIT:
+		gameExit = True
+		
+	if event.type == pygame.MOUSEBUTTONDOWN:
+		mouse_position = pygame.mouse.get_pos()
+		
+		actual_column = mouse_position[0]/60
+		actual_row = mouse_position[1]/60
+		
+		actual_coordinate = [actual_column, actual_row]
+		
+		print 'You clicked at', [actual_row, actual_column]
+		
+		for piece in gameBoard.pieces:
+			if piece.coord == actual_coordinate:
+				new_click = pygame.event.wait()
+				
+				while new_click.type != pygame.MOUSEBUTTONDOWN:
+					new_click = pygame.event.wait()
+				
+				new_position = pygame.mouse.get_pos()
+				new_coord = [new_position[0]/60, new_position[1]/60]
+				
+				if(piece.check_movement(new_coord, gameBoard)):
+					piece.move(new_coord, gameBoard)
+				else:
+					print 'Invalid movement. Try again.'
+				
+				gameBoard.get_info()
+				
+	gameBoard.update_board()			
+	pygame.display.update()
+
+pygame.quit()
+quit()
